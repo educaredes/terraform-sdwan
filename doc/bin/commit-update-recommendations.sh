dc093ff983e9fee887765770f34d09b5ca057505
daniel-gonzalez-sanchez <daniel.gonzalez.sanchez@alumnos.upm.es>
2025-12-02 16:26:37 +0100

    Adding script for updating the date of last modification for the doc/rdsv-final.md markdown file.

---
+++ doc/bin/commit-update-recommendations.sh
@@ -0,0 +1,3 @@
+date=$(date +"%d de %B de %Y")
+sed -i "s/^> Última actualización:.*/> Última actualización: $date/" ../rdsv-final.md
+git add ../rdsv-final.md && git commit -m "Updating the recommendations document for the RDSV/SDNV 25-26 final project."
